49e7473b649b4b37bea6d5373d751e54e68f7ff3
maxim <volynetsmu@gmail.com>
2023-11-24 18:51:27 +0300

    Fix bug - tests were ignored by maven

--- src/test/java/ru/volynets/test/CucumberTest.java
+++ src/test/java/ru/volynets/test/CucumberTest.java
@@ -4,10 +4,12 @@ import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
+@Suite
 @SelectClasspathResource("features")
 @IncludeEngines("cucumber")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "ru.volynets.test.steps")
-@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@shouldPass")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@base")
 public class CucumberTest {
 }
